ce966b789d569c912c0829d008284ec7a2c04df9
deepweaver <godeepweaver@gmail.com>
2019-08-22 14:30:45 -0500

    .

--- new_server.sh
+++ new_server.sh
@@ -10,10 +10,10 @@
 
 
 
-sudo apt-get install zsh 
-sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "zsh" >> ~/.bashrc
-# source ~/.bashrc
+# sudo apt-get install zsh 
+# sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# echo "zsh" >> ~/.bashrc
+# # source ~/.bashrc
 
 wget -P ~/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 cd ~
