b1a46fd54afd742e77fcd2441c970f1896f7d212
deepweaver <godeepweaver@gmail.com>
2019-08-22 15:38:59 -0500

    .

--- test_raven.py
+++ test_raven.py
@@ -63,7 +63,7 @@ device_ids = config['gpu_ids']
 if cuda:
     os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(i) for i in device_ids)
     device_ids = list(range(len(device_ids)))
-    config['gpu_ids'] = device_ids
+    config['gpu_ids'] = [3]
     cudnn.benchmark = True
 print("Arguments: {}".format(args))
 print("Use cuda: {}, use gpu_ids: {}".format(cuda, device_ids))
